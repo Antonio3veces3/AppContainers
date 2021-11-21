ecd9e3fa76bb621ffc259d2835456b0cbd0cda9b
Antonio Ramirez <cramirez28@ucol.mx>
2021-11-21 14:55:44 -0600

    Iconos agregados

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -5,12 +5,18 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Employees from '../EmployeesScreen';
 import Settings from '../SettingsScreen';
 import DetailScreen from '../DetailScreen';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import {Icon} from 'react-native-elements';
 const Tab = createBottomTabNavigator();
+
 const Component = () => {
     return (
       <Tab.Navigator
       initialRouteName= "Containers"
-      screenOptions={{headerShown: false}}>
+      screenOptions={{
+        headerShown: false,
+      }}
+      >
         <Tab.Screen name="Containers" component={DetailScreen} />
         <Tab.Screen name="Employees" component={Employees} />
         <Tab.Screen name="Settings" component={Settings} />
