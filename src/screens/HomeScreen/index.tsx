04e4446723d5983d15e60ac2e140c9a3d075f6e8
Antonio Ramirez <cramirez28@ucol.mx>
2021-11-22 23:15:07 -0600

    Resolving

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -3,44 +3,20 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 //import Container from '../../screens/ContainersScreen';
 import Employees from '../EmployeesScreen';
-import SettingsScreen from '../SettingsScreen';
-import DetailScreen from '../DetailScreen';
-import LoginScreen from '../LoginScreen';
-import {Icon} from 'react-native-elements';
+import Settings from '../SettingsScreen';
+import ContainersScreen from '../ContainersScreen';
 const Tab = createBottomTabNavigator();
 
 const Component = () => {
     return (
       <Tab.Navigator
       initialRouteName= "Containers"
-      screenOptions={({ route }) => ({
-        headerShown: false,
-        tabBarIcon: ({ focused, color }) => {
-          let iconName = '';
-
-          if (route.name === 'Containers') {
-            iconName = focused
-              ? 'truck-fast'
-              : 'truck-fast';
-          } else if (route.name === 'Settings') {
-            iconName = focused ? 'cog' : 'cog';
-          } else {
-            iconName = focused ? 'account-details' : 'account-details';
-          }
-
-          // You can return any component that you like here!
-          return <Icon type="material-community" name={iconName} size={20} color={color} />;
-        },
-        tabBarActiveTintColor: 'tomato',
-        tabBarInactiveTintColor: 'gray',
-      })}
-      >
-        <Tab.Screen name="Containers" component={DetailScreen} />
+      screenOptions={{headerShown: false}}>
+        <Tab.Screen name="Containers" component={ContainersScreen} />
         <Tab.Screen name="Employees" component={Employees} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Settings" component={Settings} />
       </Tab.Navigator>
     );
 };
 
 export default Component;
-
